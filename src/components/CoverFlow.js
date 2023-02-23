ec8ecef356b13843808c3582c4633ef042f505d0
VictorienDruon <victoriendruon@icloud.com>
2023-02-24 00:12:09 +0100

    feature : coverflow implemented with buttons

--- src/components/CoverFlow.js
+++ src/components/CoverFlow.js
@@ -4,11 +4,45 @@ import Cover from "./Cover.js"
 
 export default function CoverFlow() {
   const [musics, setMusics] = useState(musicsList);
+  const [selectedIndex, setSelectedIndex] = useState(Math.floor((musics.length - 1) / 2));
+  const selectedMusic = musics[selectedIndex];
+
+  const handlePreviousCLick = () => setSelectedIndex(prev => prev > 0 ? prev - 1 : prev);
+  const handleNextCLick = () => setSelectedIndex(prev => prev < musics.length - 1 ? prev + 1 : prev);
+
+
   return (<>
-  <div className="coverFlow" >
-    <Cover className="cover left" details={musics[0]}></Cover>
-    <Cover className="cover selected" details={musics[7]}></Cover>
-    <Cover className="cover right" details={musics[1]}></Cover>
+    <div className="cover-flow" >
+      <div className="cover-flow-left">
+        {musics.map((music, index) => {
+          if (index < selectedIndex) {
+            return <Cover key={index} className="cover left" details={music} />;
+          } else {
+            return null;
+          }
+          })}
+      </div>
+      <div className="cover-flow-selected">
+        <Cover className="cover selected" details={selectedMusic} />
+      </div>
+      <div className="cover-flow-right">
+        {musics.map((music, index) => {
+          if (index > selectedIndex) {
+            return <Cover key={index} className="cover right" details={music} />;
+          } else {
+            return null;
+          }
+        })}
+      </div>
   </div>
+  <div className="title">
+    <h3>{selectedMusic.single}<br/>{selectedMusic.artist}</h3>
+  </div>
+
+
+    <div display="flex">
+      <button onClick={handlePreviousCLick} >Previous</button>
+      <button onClick={handleNextCLick} >Next</button>
+    </div>
   </>);
 }
--- utils/musicsList.js
+++ utils/musicsList.js
@@ -3,48 +3,40 @@ export const musicsList = [
       single: "Yellow Submarine",
       artist: "The Beatles",
       cover: "/Yellow_Submarine_cover.png",
-      id: 1,
     },
     {
       single: "Grace Kelly",
       artist: "Mika",
       cover: "/life_in_a_cartoon_cover.png",
-      id: 2,
     },
     {
       single: "Are You Experienced",
       artist: "Jimi Hendrix",
       cover: "/are_you_experienced.png",
-      id: 3,
     },
     {
       single: "Billie Jean",
       artist: "Michael Jackson",
       cover: "/billie_jean.png",
-      id: 4,
     },
     {
       single: "Bohemain Rhapsody",
       artist: "Queen",
       cover: "/bohemian_rhapsody.png",
-      id: 5,
     },
     {
       single: "Viva La Vida",
       artist: "Coldplay",
       cover: "/viva_la_vida.png",
-      id: 6,
     },
     {
       single: "Under Pressure",
       artist: "David Bowie and Queen",
       cover: "/under_pressure.png",
-      id: 7,
     },
     {
       single: "Sunday Bloody Sunday",
       artist: "U2",
       cover: "/U2_Sunday_Bloody_Sunday_cover.png",
-      id: 8,
     },
   ];
