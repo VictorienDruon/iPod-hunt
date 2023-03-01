1ae718b1dffc2f69475a8f700d733a0409636cd7
VictorienDruon <victoriendruon@icloud.com>
2023-03-02 00:50:12 +0100

    feature : initial state, handle click and fixes css

--- src/components/CoverFlow.js
+++ src/components/CoverFlow.js
@@ -3,14 +3,11 @@ import {coversList} from "utils/coversList.js";
 
 export default function CoverFlow() {
   const [covers, setCovers] = useState(coversList);
-  const [selectedCover, setSelectedCover] = useState({
-    single: "Are You Experienced",
-    artist: "Jimi Hendrix",
-    cover: "/are_you_experienced.png",
-    id: 2
-  });
+  const [selectedCover, setSelectedCover] = useState(covers[Math.floor((covers.length - 1) / 2)]);
   const selectedCoverRef = useRef();
   const coverFlowRef = useRef();
+
+  useEffect(() => coverFlowRef.current.scrollTo(coverFlowRef.current.offsetWidth / 2, 0), [])
 
   const handleCoverFlowScroll = () => {
     let closestDistance = Infinity;
@@ -26,25 +23,38 @@ export default function CoverFlow() {
     setSelectedCover(closestCover);
   }
 
+  const handleCoverClick = e => {
+    const coverContainer = e.target.parentElement;
+    coverFlowRef.current.scrollTo(coverContainer.getBoundingClientRect().x + coverContainer.offsetWidth / 2, 0);
+    console.log(coverContainer)
+  }
 
   return (<>
   <div className="cover-flow" onScroll={handleCoverFlowScroll} ref={coverFlowRef} >
-    {covers.map(cover => (
+    {covers.map((cover, index) => (
       <div
         key={cover.id}
         data-id={cover.id}
-        ref={selectedCover?.id === cover.id ? selectedCoverRef : null}
-        className={`cover ${selectedCover?.id === cover.id 
+        ref={selectedCover.id === cover.id ? selectedCoverRef : null}
+        className={`cover ${selectedCover.id === cover.id 
           ? "selected"
-          : (selectedCover?.id > cover.id ? "left" : "right")}
+          : selectedCover.id > cover.id
+            ? "left"
+            : "right"}
         `}
+        style={{ zIndex: selectedCover.id === cover.id
+          ? covers.length + 1
+          : selectedCover.id < cover.id
+            ? covers.length - index
+            : undefined
+        }}
       >
-        <img src={cover.cover} alt={`${cover.single} cover`} />
+        <img src={cover.cover} alt={`${cover.single} cover`} onClick={handleCoverClick} />
       </div>
     ))}
   </div>
   <div className="title">
-    <h3>{selectedCover?.single}<br/>{selectedCover?.artist}</h3>
+    <h3>{selectedCover.single}<br/>{selectedCover.artist}</h3>
   </div>
   </>);
 }
--- utils/coversList.js
+++ utils/coversList.js
@@ -47,4 +47,148 @@ export const coversList = [
       cover: "/U2_Sunday_Bloody_Sunday_cover.png",
       id: 7
     },
+    {
+      single: "Yellow Submarine",
+      artist: "The Beatles",
+      cover: "/Yellow_Submarine_cover.png",
+      id: 8,
+    },
+    {
+      single: "Grace Kelly",
+      artist: "Mika",
+      cover: "/life_in_a_cartoon_cover.png",
+      id: 9
+    },
+    {
+      single: "Are You Experienced",
+      artist: "Jimi Hendrix",
+      cover: "/are_you_experienced.png",
+      id: 10
+    },
+    {
+      single: "Billie Jean",
+      artist: "Michael Jackson",
+      cover: "/billie_jean.png",
+      id: 11
+    },
+    {
+      single: "Bohemain Rhapsody",
+      artist: "Queen",
+      cover: "/bohemian_rhapsody.png",
+      id: 12
+    },
+    {
+      single: "Viva La Vida",
+      artist: "Coldplay",
+      cover: "/viva_la_vida.png",
+      id: 13
+    },
+    {
+      single: "Under Pressure",
+      artist: "David Bowie and Queen",
+      cover: "/under_pressure.png",
+      id: 14
+    },
+    {
+      single: "Sunday Bloody Sunday",
+      artist: "U2",
+      cover: "/U2_Sunday_Bloody_Sunday_cover.png",
+      id: 15
+    },
+    {
+      single: "Yellow Submarine",
+      artist: "The Beatles",
+      cover: "/Yellow_Submarine_cover.png",
+      id: 16,
+    },
+    {
+      single: "Grace Kelly",
+      artist: "Mika",
+      cover: "/life_in_a_cartoon_cover.png",
+      id: 17
+    },
+    {
+      single: "Are You Experienced",
+      artist: "Jimi Hendrix",
+      cover: "/are_you_experienced.png",
+      id: 18
+    },
+    {
+      single: "Billie Jean",
+      artist: "Michael Jackson",
+      cover: "/billie_jean.png",
+      id: 19
+    },
+    {
+      single: "Bohemain Rhapsody",
+      artist: "Queen",
+      cover: "/bohemian_rhapsody.png",
+      id: 20
+    },
+    {
+      single: "Viva La Vida",
+      artist: "Coldplay",
+      cover: "/viva_la_vida.png",
+      id: 21
+    },
+    {
+      single: "Under Pressure",
+      artist: "David Bowie and Queen",
+      cover: "/under_pressure.png",
+      id: 22
+    },
+    {
+      single: "Sunday Bloody Sunday",
+      artist: "U2",
+      cover: "/U2_Sunday_Bloody_Sunday_cover.png",
+      id: 23
+    },
+    {
+      single: "Yellow Submarine",
+      artist: "The Beatles",
+      cover: "/Yellow_Submarine_cover.png",
+      id: 24,
+    },
+    {
+      single: "Grace Kelly",
+      artist: "Mika",
+      cover: "/life_in_a_cartoon_cover.png",
+      id: 25
+    },
+    {
+      single: "Are You Experienced",
+      artist: "Jimi Hendrix",
+      cover: "/are_you_experienced.png",
+      id: 26
+    },
+    {
+      single: "Billie Jean",
+      artist: "Michael Jackson",
+      cover: "/billie_jean.png",
+      id: 27
+    },
+    {
+      single: "Bohemain Rhapsody",
+      artist: "Queen",
+      cover: "/bohemian_rhapsody.png",
+      id: 28
+    },
+    {
+      single: "Viva La Vida",
+      artist: "Coldplay",
+      cover: "/viva_la_vida.png",
+      id: 29
+    },
+    {
+      single: "Under Pressure",
+      artist: "David Bowie and Queen",
+      cover: "/under_pressure.png",
+      id: 30
+    },
+    {
+      single: "Sunday Bloody Sunday",
+      artist: "U2",
+      cover: "/U2_Sunday_Bloody_Sunday_cover.png",
+      id: 31
+    },
   ];
